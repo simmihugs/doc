82c40c39ab5466fa258cc11e3172b87e37edee69
simmihugs <simon.graetz@gmx.de>
2025-04-18 18:27:22 +0200

    Added svg mouth

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,12 @@ import { useState, useRef } from "react";
 import "./App.css";
 import "./Doc.css";
 
-const eyeLeft = { x: 30 + 25, y: 30 + 15 };
-const eyeRight = { x: 180 - 30 - 25, y: 30 + 15 };
-const pupilRadius = 8;
-const directions = {
-  left: Math.PI,
-  right: 0,
-  up: -Math.PI / 2,
-  down: Math.PI / 2,
+const MOUTH_PATHS = {
+  frown: "M 30 70 Q 50 40 70 70",
+  neutral: "M 30 60 L 70 60",
+  smile: "M 30 60 Q 50 90 70 60"
 };
-
-function getPupilStyle(center, angle) {
-  return {
-    left: center.x + pupilRadius * Math.cos(angle) - 10,
-    top: center.y + pupilRadius * Math.sin(angle) - 7.5,
-    position: "absolute",
-  };
-}
-
-function getDirectionStyle(center, direction) {
-  if (!directions.hasOwnProperty(direction)) return {};
-
-  return {
-    left: center.x + pupilRadius * Math.cos(directions[direction]) - 10,
-    top: center.y + pupilRadius * Math.sin(directions[direction]) - 7.5,
-  };
-}
+const EXPRESSION_ORDER = ['frown', 'neutral', 'smile'];
 
 function _handleMoveEyesDirection({
   direction,
@@ -89,13 +69,20 @@ export default function App() {
   const [isBlinking, setIsBlinking] = useState(false);
   const [isBlinkingLeft, setIsBlinkingLeft] = useState(false);
   const [isBlinkingRight, setIsBlinkingRight] = useState(false);
-  const [isRolling, setIsRolling] = useState(false);
-  const [pupilAngle, setPupilAngle] = useState(0);
-  const [pupilDirection, setPupilDirection] = useState("center");
   const BASE_POSITION = { x: 45, y: 38 };
   const [position, setPosition] = useState(BASE_POSITION);
   const moveAmount = 10;
   const timeoutRef = useRef(null);
+  const [mouthExpression, setMouthExpression] = useState('neutral');
+
+  const handleButtonClick = () => {
+    const currentIndex = EXPRESSION_ORDER.indexOf(mouthExpression);
+    console.log("currentIndex: ", currentIndex);
+    const nextIndex = (currentIndex + 1) % EXPRESSION_ORDER.length;
+    console.log("nextIndex: ", nextIndex);
+    setMouthExpression(EXPRESSION_ORDER[nextIndex]);
+  };
+
 
   function handleBlink({ left = false, right = false }) {
     _handleBlink({
@@ -133,24 +120,20 @@ export default function App() {
     <div className="doctor-container">
       <div className="doctor-head">
         <div
-          className={`doctor-eyelid left top ${
-            isBlinkingLeft && isBlinking ? "closed" : ""
-          }`}
+          className={`doctor-eyelid left top ${isBlinkingLeft && isBlinking ? "closed" : ""
+            }`}
         ></div>
         <div
-          className={`doctor-eyelid left bottom ${
-            isBlinkingLeft && isBlinking ? "closed" : ""
-          }`}
+          className={`doctor-eyelid left bottom ${isBlinkingLeft && isBlinking ? "closed" : ""
+            }`}
         ></div>
         <div
-          className={`doctor-eyelid right top ${
-            isBlinkingRight && isBlinking ? "closed" : ""
-          }`}
+          className={`doctor-eyelid right top ${isBlinkingRight && isBlinking ? "closed" : ""
+            }`}
         ></div>
         <div
-          className={`doctor-eyelid right bottom ${
-            isBlinkingRight && isBlinking ? "closed" : ""
-          }`}
+          className={`doctor-eyelid right bottom ${isBlinkingRight && isBlinking ? "closed" : ""
+            }`}
         ></div>
 
         <div className="doctor-eye left"></div>
@@ -171,9 +154,24 @@ export default function App() {
           }}
         />
 
-        <div className="doctor-mouth"></div>
+        <svg className="w-24 h-24 absolute bottom-4" viewBox="0 0 100 100">
+          <path
+            d={MOUTH_PATHS[mouthExpression]}
+            stroke="black"
+            strokeWidth="5"
+            fill="transparent"
+            strokeLinecap="round"
+          />
+        </svg>
+
       </div>
       <div className="doctor-buttons">
+        <button
+          onClick={handleButtonClick}
+          className="doctor-blink"
+        >
+          Change Expression
+        </button>
         <button
           className="doctor-blink"
           onClick={() => handleMoveEyesDirection("left")}
